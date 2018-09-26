ddd23edf1ca343cf343badd0c311bb9b900ed701
LagrangedWho <517862788@qq.com>
2018-09-26 16:45:19 +0800

    a python3 script of logining nju-wlan
    No third-party python library. Update ```sid``` and ```pw``` before running.

---
+++ login_nju_wlan_py3.py
@@ -0,0 +1,13 @@
+#/bin/python3
+
+from urllib import request
+from urllib import parse
+
+sid = "MF1833026"
+pw = ""
+postdata = {"username":sdi, "password":pw}
+url = 'http://p.nju.edu.cn/portal_io/login'
+postdata = parse.urlencode(postdata).encode('utf-8')
+new_url = request.Request(url, postdata)
+response = request.urlopen(new_url)
+print(response.read())
