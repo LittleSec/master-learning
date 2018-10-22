016fa85d24eadbb9c5f61aa392ebfad266ab3ea5
LittleSec <517862788@qq.com>
2018-10-22 16:56:45 +0800

    fix chinese utf-8 problem
    巧合地，中文字符的utf8编码都是3个字节。

--- nlp_homework/Proj2/proj2.cpp
+++ nlp_homework/Proj2/proj2.cpp
@@ -44,9 +44,9 @@ class Dict{
         void showDict(int num=10);
         ~Dict();
         // should be private
-        map<string, vector<string> > dict;
     private:
         long dict_size;
+        map<string, vector<string> > dict;
         // vector<string> dict;
         string filename;
 };
@@ -68,17 +68,18 @@ bool Dict::isInDict(string& s){
 void Dict::setDict(string& filename){
     ifstream dict_file;
     dict_file.open(filename, ios::in);
+    dict_file.imbue(locale("zh_CN.UTF-8"));
     string s;
     string cn;
-    vector<string> en;
+    vector<string> en_vector;
     while(getline(dict_file, s))
     {
         // cout << s << endl;
-        en.clear();
-        string::size_type cn_en_pos = s.find(split_char);
+        en_vector.clear();
+        string::size_type cn_en_pos = s.find(split_char); // 必有一个split_char即默认不存在找不到的情况
         cn = s.substr(0, cn_en_pos);
-        SplitString(s.substr(cn_en_pos), en, split_char);
-        dict.insert(make_pair(cn, en));
+        SplitString(s.substr(cn_en_pos+1), en_vector, split_char);
+        dict.insert(make_pair(cn, en_vector));
     }
     dict_file.close();
 }
@@ -108,64 +109,69 @@ Dict::~Dict(){
     dict.clear();
 }
 
-void RMM(string seq, Dict d, vector<string>& splic_word_res){
-    string::size_type i = seq.size();
-    while(i > 0){
-        for(string::size_type j = 0; j < seq.size(); j++){
-            string tmp = seq.substr(j, i);
-            if(tmp.size() == 1){
+void FMM(string& seq, Dict& d, vector<string>& splic_word_res){
+    // string::size_type是无符号，意味着：如果是<0作为条件的话，是永真的！
+    int i = 0;
+    string tmp;
+    while(i < seq.length()){ 
+        // 不能j = seq.length()/3，因为substr()不通用！
+        for(int j = seq.length(); j > i; j-=3){
+            tmp = seq.substr(i, j-i); // substr(start, length)
+            // cout << i << " " <<  j << " " << tmp << endl;  
+            if(tmp.length() == 3){
                 splic_word_res.push_back(tmp);
             }
             else if(d.isInDict(tmp)){
                 splic_word_res.push_back(tmp);
-                i = i - (tmp.size()-1);
+                i = i + (tmp.length() - 3);
             }
         }
-        i--;
+        i+=3;
+    }
+}
+
+void RMM(string& seq, Dict& d, vector<string>& splic_word_res){
+    int i = seq.length();
+    string tmp;
+    while(i > 0){
+        for(int j = 0; j < seq.length(); j+=3){
+            if(i <= j){
+                break;
+            }
+            tmp = seq.substr(j, i-j);
+            if(tmp.length() == 3){
+                splic_word_res.push_back(tmp);
+            }
+            else if(d.isInDict(tmp)){
+                splic_word_res.push_back(tmp);
+                i = i - (tmp.length()-3);
+            }
+        }
+        i-=3;
     }
     reverse(splic_word_res.begin(), splic_word_res.end());
 }
 
-void FMM(string seq, Dict d, vector<string>& splic_word_res){
-    string::size_type i = 0;
-    while(i < seq.size()){
-        for(string::size_type j = seq.size(); j >= 0; j--){
-            string tmp = seq.substr(i, j);
-            if(tmp.size() == 1){
-                splic_word_res.push_back(tmp);
-            }
-            else if(d.isInDict(tmp)){
-                splic_word_res.push_back(tmp);
-                i = i + (tmp.size() - 1);
-            }
-        }
-        i++;
-    }
-}
-
 int main(int argc, char const *argv[]){
-    string sequence;
+    string sequence = "细胞分裂组织";
     string filename = "ce.txt";
     Dict d(filename);
     vector<string> fm;
     vector<string> rm;
-    cin >> sequence;
-    // for(map<string, vector<string> >::iterator iter = d.dict.begin(); iter != d.dict.end(); iter++){
-    //     cout << iter->first << endl;
-    // } 
-    cout << d.dict.size() << endl;
-    d.showDict(10);
-    /*
-    RMM(sequence, d, rm);
+    // cin >> sequence;
+    // cout << d.dict.size() << endl;
+    // d.showDict(10);
     FMM(sequence, d, fm);
     for(vector<string>::iterator it = fm.begin(); it != fm.end(); it++){
         cout << *it << '/';
     }
     cout << endl;
-    for(vector<string>::iterator it = rm.begin(); it != fm.end(); it++){
+
+    RMM(sequence, d, rm);
+    for(vector<string>::iterator it = rm.begin(); it != rm.end(); it++){
         cout << *it << '/';
     }
     cout << endl;
-    */
+
     return 0;
 }
